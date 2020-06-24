d544177a63ff1241d84b7a8fe5e1e4e79fb4c757
tjdtjq5 <tjdtjq5@naver.com>
2020-06-25 06:33:20 +0900

    tjdtjq

---
+++ Assets/Script/BackEndNickname.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using BackEnd;
+using System.Text.RegularExpressions;
+
+public class BackEndNickname : MonoBehaviour
+{
+    public InputField nickInput;
+
+    // 한글, 영어, 숫자만 입력 가능하게
+    private bool CheckNickname()
+    {
+        return Regex.IsMatch(nickInput.text, "^[0-9a-zA-Z가-힣]*$");
+    }
+
+    public void OnClickCreateName()
+    {
+        // 한글, 영어, 숫자로만 닉네임을 만들었는지 체크
+        if (CheckNickname() == false)
+        {
+            Debug.Log("닉네임은 한글, 영어, 숫자로만 만들 수 있습니다.");
+            return;
+        }
+
+        BackendReturnObject BRO = Backend.BMember.CreateNickname(nickInput.text);
+
+        if (BRO.IsSuccess())
+        {
+            Debug.Log("닉네임 생성 완료");
+        }
+
+        else
+        {
+            switch (BRO.GetStatusCode())
+            {
+                case "409":
+                    Debug.Log("이미 중복된 닉네임이 있는 경우");
+                    break;
+
+                case "400":
+                    if (BRO.GetMessage().Contains("too long")) Debug.Log("20자 이상의 닉네임인 경우");
+                    else if (BRO.GetMessage().Contains("blank")) Debug.Log("닉네임에 앞/뒤 공백이 있는경우");
+                    break;
+
+                default:
+                    Debug.Log("서버 공통 에러 발생: " + BRO.GetErrorCode());
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Script/NetworkTime.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Networking;
+
+public class NetworkTime : MonoBehaviour
+{
+    public string url = "";
+
+    void Start()
+    {
+
+        StartCoroutine(WebChk());
+
+    }
+    TimeSpan timestamp;
+    IEnumerator WebChk()
+    {
+        UnityWebRequest request = new UnityWebRequest();
+        using (request = UnityWebRequest.Get(url))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.isNetworkError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                string date = request.GetResponseHeader("date");
+
+                if (date.Contains("Mon"))
+                {
+
+                }
+
+                DateTime dateTime = DateTime.Parse(date).ToUniversalTime();
+              
+                timestamp = dateTime - new DateTime(1970, 1, 1, 0, 0, 0);
+
+                int stopwatch =
+                    (int)timestamp.TotalSeconds - PlayerPrefs.GetInt("net", (int)timestamp.TotalSeconds);
+
+                Debug.Log(stopwatch + "sec");
+              
+            }
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.SetInt("net", (int)timestamp.TotalSeconds);
+    }
+}
